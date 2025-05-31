04a423c4bb5997b26b20c56cc41ee7b895c3544f
CarloPace <carloppace@outlook.it>
2025-05-31 02:41:32 +0200

    Improved global search with filters

--- src/main/java/com/example/demo/services/MongoDB/GlobalSearch_service.java
+++ src/main/java/com/example/demo/services/MongoDB/GlobalSearch_service.java
@@ -11,9 +11,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.regex.Pattern;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.ListIterator;
 
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
@@ -33,8 +40,25 @@ public class GlobalSearch_service {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+    private static final Map<String, String> collectionNameMappings;
+    private static final Map<String, String> collectionAttributeMappings;
+    
+    static {
+        collectionAttributeMappings = new HashMap<>(); // Initialize the HashMap
+        collectionAttributeMappings.put("user","username");
+        collectionAttributeMappings.put("team","team_name");
+        collectionAttributeMappings.put("coach", "long_name");
+        collectionAttributeMappings.put("player","long_name");
+        
+        collectionNameMappings = new HashMap<>(); // Initialize the HashMap
+        collectionNameMappings.put("user","Users");
+        collectionNameMappings.put("team","Teams");
+        collectionNameMappings.put("coach", "Coaches");
+        collectionNameMappings.put("player","Players");
+    }
 
-    public Page<globalSearchResult> globalSearch(String keyword, Pageable pageable) {
+    // Version 1
+   /* public Page<globalSearchResult> globalSearch(String keyword, Pageable pageable) {
         
        Pattern pattern = Pattern.compile(keyword, Pattern.CASE_INSENSITIVE);
         Criteria commonCriteria = new Criteria().orOperator(
@@ -100,6 +124,75 @@ public class GlobalSearch_service {
                 .toList());
                 
         AggregationResults<facetResultDTO> results = mongoTemplate.aggregate(aggregation, "Users", facetResultDTO.class);
+        facetResultDTO facetResult = results.getUniqueMappedResult();
+
+        List<globalSearchResult> content = facetResult != null && facetResult.getData() != null
+                ? facetResult.getData()
+                : Collections.emptyList();
+
+        long total = (facetResult != null && facetResult.getCount() != null && !facetResult.getCount().isEmpty())
+                ? facetResult.getCount().get(0).getCount()
+                : 0L;
+
+        return new PageImpl<>(content, pageable, total);
+    }*/
+    // Version 2
+    public Page<globalSearchResult> globalSearch(String keyword, Pageable pageable, List<String> searchInCollections) {
+
+        Pattern pattern = Pattern.compile(keyword, Pattern.CASE_INSENSITIVE);
+        
+        if(searchInCollections.isEmpty())
+            searchInCollections.addAll(Arrays.asList("user", "coach", "team", "player"));
+        
+
+        List<Document> pipeline = new ArrayList<>();
+        ListIterator<String> iterator = searchInCollections.listIterator();
+        
+        String collection=iterator.next();
+        
+        
+        pipeline.add(new Document("$match", new Document(collectionAttributeMappings.get(collection), pattern)));
+        pipeline.add(new Document("$project", new Document("mongo_id", "$_id")
+                .append("name", "$"+collectionAttributeMappings.get(collection))
+                .append("type", collection)));
+
+        // Conditional $unionWith stages
+        while(iterator.hasNext()){
+            collection=iterator.next();
+            pipeline.add(new Document("$unionWith", new Document("coll", collectionNameMappings.get(collection))
+                    .append("pipeline", List.of(
+                            new Document("$match", new Document(collectionAttributeMappings.get(collection), pattern)),
+                            new Document("$project", new Document("mongo_id", "$_id")
+                                    .append("name", "$"+collectionAttributeMappings.get(collection))
+                                    .append("type", collection))
+                    ))
+            ));
+        }
+        
+        if (pipeline.isEmpty()) {
+            return new PageImpl<>(Collections.emptyList(), pageable, 0L);
+        }
+        
+
+        // $facet for pagination and count
+        Document facetStage = new Document("$facet", new Document()
+                .append("data", List.of(
+                        new Document("$skip", pageable.getOffset()),
+                        new Document("$limit", pageable.getPageSize())
+                ))
+                .append("count", List.of(
+                        new Document("$count", "count")
+                ))
+        );
+        pipeline.add(facetStage);
+
+        // Run aggregation
+        Aggregation aggregation = Aggregation.newAggregation(pipeline.stream()
+                .map(stage -> (AggregationOperation) context -> stage)
+                .toList());
+        
+        
+        AggregationResults<facetResultDTO> results = mongoTemplate.aggregate(aggregation,collectionNameMappings.get(collection), facetResultDTO.class);
         facetResultDTO facetResult = results.getUniqueMappedResult();
 
         List<globalSearchResult> content = facetResult != null && facetResult.getData() != null
